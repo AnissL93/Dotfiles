e0799c59c818ace7097b91fd5a9440f0be0427d8
Auii <saintansis@gmail.com>
2022-03-01 00:58:57 +0800

    update emacs noter

--- config/doom-emacs/config.el
+++ config/doom-emacs/config.el
@@ -300,9 +300,9 @@
    ;; The WM can handle splits
    org-noter-notes-window-location 'other-frame
    ;; Please stop opening frames
-   org-noter-always-create-frame nil
+   org-noter-always-create-frame t
    ;; I want to see the whole file
-   org-noter-hide-other t
+   org-noter-hide-other nil
    ;; Everything is relative to the main notes file
    org-noter-notes-search-path (list org_notes)
    )
